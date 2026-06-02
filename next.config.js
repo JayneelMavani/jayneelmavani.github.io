be988f090f91d631760b57ae14c730ab1bea9086
Jayneel Mavani <169598522+radiant82@users.noreply.github.com>
2026-06-03 01:10:35 +0530

    add CI and deployment workflows for GitHub Pages, update Next.js configuration

--- next.config.js
+++ next.config.js
@@ -1,8 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
   swcMinify: true,
   compress: true,
   poweredByHeader: false,
+  images: {
+    unoptimized: true,
+  },
 };
 
 module.exports = nextConfig;
